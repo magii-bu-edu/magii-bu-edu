c00f0c5fbfcc4ea2583a02cc769872e17a027f7e
magii-bu-edu <magii@bu.edu>
2024-06-17 19:02:08 +0000

    ahflaflhkwhgt

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ import random
 def get_random_word():
     with open("words.txt") as file:
         return random.choice(list(file))[:-1].lower()
-    
-print(get_random_word())
+
+def play_unscramble():
+
+    while True:
+        summer_word = get_random_word()
+        print(summer_word)
+        break
+
+
+#introduction()
+play_unscramble()
